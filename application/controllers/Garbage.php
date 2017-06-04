707813af6ef02364290465ad015661108ff03049
ll3an <vayu.net@gmail.com>
2017-06-04 18:05:58 +0700

    new git

--- application/controllers/Garbage.php
+++ application/controllers/Garbage.php
@@ -18,6 +18,14 @@ class Garbage extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('garbage_model');
+	}
+
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
